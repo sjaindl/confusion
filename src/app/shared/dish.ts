ea2cdcd1227adc7ada4d84574fb7045e66405b81
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-02-06 22:00:59 +0100

    data binding

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+import { Comments } from "./comments";
+
+export class Dish {
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+    comments: Comments[];
+}
