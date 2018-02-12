b256222a888aa3f171a1d30118e1e9fd90f17069
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-02-12 22:16:55 +0100

    Assignment 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,6 +13,34 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+  };
+
   constructor(private formBuilder: FormBuilder) {
     this.createForm();
    }
@@ -22,14 +50,36 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.formBuilder.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
-      contacttype: this.contactType[0],
+      contacttype: 'None',
       message: ''
     });
+
+    this.onValueChanged(); // (re)set validation messages now
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { 
+      return; 
+    }
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = ''; // clear previous error message (if any)
+      const control = this.feedbackForm.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   onSubmit() {
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,10 @@ import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import 'rxjs/add/operator/switchMap';
+import { Comments } from '../shared/comments';
 
 @Component({
   selector: 'app-dishdetail',
@@ -14,18 +18,100 @@ export class DishdetailComponent implements OnInit {
 
   // @Input()
   dish: Dish;
+  dishIds: number[];
+  prev: number;
+  next: number;
+
+  contactForm: FormGroup;
+  comment: Comments;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Author is required.',
+      'minlength':     'Author must be at least 2 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.'
+    }
+  };
 
   constructor( 
     private dishService : DishService,
     private route : ActivatedRoute,
-    private location : Location) {
+    private location : Location,
+    private formBuilder: FormBuilder) {
+      this.createForm();
    }
 
   ngOnInit() {
-    let id = +this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-      .subscribe(dish => this.dish = dish);
-    alert(id + " " + this.dish.name);
+    //let id = +this.route.snapshot.params['id'];
+    
+    //this.dishService.getDish(id)
+      //.subscribe(dish => this.dish = dish);
+      
+    this.dishService.getDishIds()
+      .subscribe(ids => this.dishIds = ids);
+    
+    this.route.params
+      .switchMap((params: Params) => this.dishService.getDish(+params['id']))
+      .subscribe(dish => { 
+        this.dish = dish; this.setPrevNext(dish.id); 
+      });
+
+  }
+
+  createForm() {
+    this.contactForm = this.formBuilder.group({
+      author: ['', [Validators.required, Validators.minLength(2)] ],
+      comment: ['', Validators.required ],
+      rating: 5
+    });
+
+    this.onValueChanged(); // (re)set validation messages now
+
+    this.contactForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.contactForm) { 
+      return; 
+    }
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = ''; // clear previous error message (if any)
+      const control = this.contactForm.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.comment = this.contactForm.value;
+    this.comment.date = new Date(Date.now()).toDateString();
+    
+    this.dish.comments.push(this.comment);
+
+    this.contactForm.reset({
+      author: '',
+      comment: '',
+      rating: 5
+    });
+  }
+
+  setPrevNext(dishId: number) {
+    let index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
   }
 
   goBack() : void {
